97b2663e7a0d7bfd086407bbfb94ec7d0e1a81d9
DRD <Saxena0794@gmail.com>
2026-06-12 11:36:31 +0530

    App commit code and use in 12/06/2026 11.36 AM

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,16 +51,6 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
 
-
-//    // CameraX
-//    implementation("androidx.camera:camera-camera2:1.3.0")
-//    implementation("androidx.camera:camera-lifecycle:1.3.0")
-//    implementation("androidx.camera:camera-view:1.3.0")
-//// ML Kit (Face Detection)
-//    implementation("com.google.mlkit:face-detection:16.1.5")
-//// TensorFlow Lite
-//    implementation("org.tensorflow:tensorflow-lite:2.12.0")
-//    implementation("org.tensorflow:tensorflow-lite-support:0.4.3")
     implementation("androidx.compose.material:material-icons-extended:1.6.0")
     implementation(libs.androidx.room3.common.jvm)
     implementation(libs.androidx.compose.material3.window.size.class1)
